24370fb85e197a77e6a3038c3d0307de77f88746
Daniel Castaño Rodríguez <UO288241@uniovi.es>
2024-12-26 16:42:18 +0100

    Añadir visualizaciones a la aplicación web.
    Añadir gráficos interactivos de plotly para visualizar los datos temporales.

--- web-app/app.py
+++ web-app/app.py
@@ -1,7 +1,8 @@
 import sqlite3
 import pandas as pd
 import streamlit as st
-from matplotlib import pyplot as plt
+import plotly.express as px
+
 
 database_path = '/db/data.db'
 
@@ -16,19 +17,48 @@ def query_data(table):
             df['timestamp'] = pd.to_datetime(df['timestamp'])
             return df
     except Exception as e:
-        st.write(f'Error al obtener los datos de la base de datos: {e}')
+        return pd.DataFrame()
 
 
-st.write('Web app')
+st.title("Huerto inteligente")
 
-df = query_data('humidity')
+df_humidity = query_data('humidity')
+df_humidity['sensor'] = 'Humedad ambiente'
+df_soil_moisture = query_data('soil_moisture')
+df_soil_moisture['sensor'] = 'Humedad del suelo'
+df_temperature = query_data('temperature')
+df_temperature['sensor'] = 'Temperatura'
+df_altitude = query_data('altitude')
+df_altitude['sensor'] = 'Altitud'
+df_pressure = query_data('pressure')
+df_pressure['sensor'] = 'Presión'
 
-df = df[df['controller_id'] == 1]
+df = pd.concat([df_humidity, df_soil_moisture, df_temperature,
+               df_altitude, df_pressure], axis=0)
 
-fig, ax = plt.subplots(figsize=(10, 5))
+controller_id = st.selectbox(
+    'Selecciona Microcontrolador:', options=df['controller_id'].unique())
+sensor = st.selectbox('Selecciona Tipo de Sensor:',
+                      options=df['sensor'].unique())
 
-ax.plot(df['timestamp'], df['value'])
-ax.set_title('Serie Temporal')
-ax.set_xlabel('Timestamp')
-ax.set_ylabel('Value')
-st.pyplot(fig)
+filtered_df = df[(df['controller_id'] == controller_id)
+                 & (df['sensor'] == sensor)]
+
+min_fecha = filtered_df['timestamp'].min()
+max_fecha = filtered_df['timestamp'].max()
+inicio_rango = max_fecha - pd.Timedelta(days=1)
+
+fig = px.line(
+    filtered_df,
+    x='timestamp',
+    y='value',
+    title=f"Serie Temporal - Micro {controller_id}, Sensor {sensor}",
+    labels={'value': 'Valor', 'timestamp': 'Fecha'}
+)
+
+fig.update_xaxes(
+    range=[inicio_rango, max_fecha],
+    rangeslider_visible=True
+)
+
+st.plotly_chart(fig)
