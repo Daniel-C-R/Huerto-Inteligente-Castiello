6014e6d049c26d451fc216afc02ff93e74805e1a
Daniel-C-R <UO288241@uniovi.es>
2025-01-22 14:52:50 +0100

    Mejoras varias del código del ESP32.
    Se ha limpiado un poco el código y mejorado los comentarios y mensajes
    por el puerto serial. También, se han añadido unas macros al principio
    del código con los parámetros de configuración (id del controlador,
    parámetros para las conexiones, valores de calibración del higrómetro,
    etc) para poder cambiarlos con facilidad. A su vez, se ha añadido
    feedback visual en el LED RGB para el proceso de configuración. Se ha
    eliminado la funcionalidad para suscribirse.

--- esp32/program.ino
+++ esp32/program.ino
@@ -5,57 +5,75 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-// Definir los pines I2C
+// Establecer ID del microcontrolador
+#define CONTROLLER_ID 1
+
+// Configuración y calibración del higrómetro capacitivo
+#define SOIL_MOISTURE_PIN A0    // Pin del que se leen los datos
+#define DRY_SOIL_MOISTURE 2364  // Valor que se considera 0% de humedad
+#define WET_SOIL_MOISTURE 1289  // Valor que se considera 100% de humedad
+
+// Definir el brillo máximo del LED
+#define RGB_BRIGHTNESS 255
+
+// Configuración de la red WiFi
+#define SSID "CAMBIAR"
+#define PASS "CAMBIAR"
+
+// Configuración del broker de MQTT
+#define MQTT_SERVER "CAMBIAR"
+#define MQTT_PORT 1883
+
+// Definir los pines I2C para el BME280
 #define I2C_SCL 20
 #define I2C_SDA 21
 
-// Configuración de la red WiFi
-const char* ssid = "CAMBIAR";
-const char* password = "CAMBIAR";
+// Temas en los que publicar
+const char* topics[] = {
+  "huerto/temperature",
+  "huerto/pressure",
+  "huerto/altitude",
+  "huerto/humidity",
+  "huerto/soil_moisture"
+};
 
-// Configuración del broker MQTT
-const char* mqtt_server = "CAMBIAR"; // IP del broker MQTT
-const int mqtt_port = 1883;               // Puerto del broker MQTT
-
-// Configuración del sensor BME280
+// Objeto BME280
 Adafruit_BME280 bme;
-
-// Configuración del sensor de humedad del suelo
-#define SOIL_MOISTURE_PIN A0 // Pin analógico donde conectas el sensor
-const int MOISTURE_AIR = 2634; // Valor leído en suelo seco
-const int MOISTURE_WATER = 1289;    // Valor leído en suelo húmedo
 
 // Cliente WiFi y MQTT
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup_wifi() {
+// Conexión a WiFi
+void setupWiFi() {
   delay(10);
   Serial.println();
   Serial.print("Conectando a WiFi...");
-  
-  WiFi.begin(ssid, password);
+
+  WiFi.begin(SSID, PASS);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println(" Conectado a WiFi");
+  Serial.print("\n");
+  Serial.print("Conectado correctamente a la red WiFi: ");
+  Serial.print(SSID);
+  Serial.print("\n");
 }
 
-void reconnect() {
+// Conexión al broker de MQTT
+void connectMQTT() {
+  Serial.println("Conectando al broker de MQTT...");
+
   while (!client.connected()) {
-    Serial.print("Intentando conectar al broker MQTT...");
     if (client.connect("ESP32Client")) {
-      Serial.println(" Conectado");
-      for (const char* topic : topics) {
-        client.subscribe(topic);
-        Serial.println(String("Suscrito al tema: ") + topic);
-      }
+      Serial.print("\nConectado al broker MQTT con dirección: ");
+      Serial.println(MQTT_SERVER);
     } else {
-      Serial.print("Error: ");
+      Serial.print("Falló la conexión. Código de error: ");
       Serial.print(client.state());
-      Serial.println(" Reintentando en 5 segundos");
+      Serial.print(" Reintentando en 5 segundos\n");
       delay(5000);
     }
   }
@@ -64,12 +82,12 @@ void reconnect() {
 // Función para construir y publicar datos JSON
 void publishData(const char* topic, float value, const char* key) {
   StaticJsonDocument<200> jsonDoc;
-  jsonDoc["controller_id"] = 2;
+  jsonDoc["controller_id"] = CONTROLLER_ID;
   jsonDoc[key] = value;
 
   char jsonBuffer[200];
   serializeJson(jsonDoc, jsonBuffer);
-  
+
   client.publish(topic, jsonBuffer);
   Serial.print("Publicado en ");
   Serial.print(topic);
@@ -78,17 +96,32 @@ void publishData(const char* topic, float value, const char* key) {
 }
 
 void setup() {
+  // La luz blanca indica que ha comenzado el proceso de setup
+  rgbLedWrite(RGB_BUILTIN, RGB_BRIGHTNESS, RGB_BRIGHTNESS, RGB_BRIGHTNESS);
+
   Serial.begin(115200);
+  Serial.println("Iniciando el proceso de configuración.");
+  Serial.print("Microcontrolador con ID: ");
+  Serial.println(CONTROLLER_ID);
 
-  setup_wifi();
+  // Conectar WiFi
+  setupWiFi();
 
-  client.setServer(mqtt_server, mqtt_port);
+  // Configurar broker MQTT
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  connectMQTT();
+
+  // La luz azul oscuro indica que la conexión al broker ha sido
+  rgbLedWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS);
 
   // Inicializar el sensor BME280
   Wire.begin(I2C_SDA, I2C_SCL);
   if (!bme.begin(0x76)) {
-    Serial.println(F("No se pudo encontrar un sensor BME280 válido, ¡verifica el cableado!"));
-    while (1);
+    Serial.println("No se pudo encontrar un sensor BME280 válido, ¡verifica el cableado!");
+    // Indicamos en magenta que el BME280 no está bien conectado
+    rgbLedWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, RGB_BRIGHTNESS);
+    while (1)
+      ;
   }
 
   // Configuración del pin de humedad del suelo
@@ -97,7 +130,7 @@ void setup() {
 
 void loop() {
   if (!client.connected()) {
-    reconnect();
+    connectMQTT();
   }
   client.loop();
 
@@ -109,15 +142,14 @@ void loop() {
 
     // Leer datos del BME280
     float temperature = bme.readTemperature();
-    float pressure = bme.readPressure() / 100.0F; // Conversión a hPa
-    float altitude = bme.readAltitude(1013.25);  // Ajusta según la presión local
+    float pressure = bme.readPressure() / 100.0F;  // Conversión a hPa
+    float altitude = bme.readAltitude(1013.25);    // Ajusta según la presión local
     float humidity = bme.readHumidity();
 
     // Leer datos de humedad del suelo
     int analogValue = analogRead(SOIL_MOISTURE_PIN);
-    Serial.println(analogValue);
-    float soilMoisture = map(analogValue, MOISTURE_AIR, MOISTURE_WATER, 0, 100);
-    soilMoisture = constrain(soilMoisture, 0, 100); // Limitar a valores entre 0 y 100%
+    float soilMoisture = map(analogValue, DRY_SOIL_MOISTURE, WET_SOIL_MOISTURE, 0, 100);
+    soilMoisture = constrain(soilMoisture, 0, 100);  // Limitar a valores entre 0 y 100%
 
     // Publicar datos en formato JSON
     publishData("huerto/temperature", temperature, "temperature");
@@ -126,5 +158,11 @@ void loop() {
     publishData("huerto/humidity", humidity, "humidity");
     publishData("huerto/soil_moisture", soilMoisture, "soil_moisture");
 
+    // Decisión de riego
+    if (soilMoisture < 70 || (temperature > 25 && soilMoisture < 85)) {
+      rgbLedWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0);
+    } else {
+      rgbLedWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);
+    }
   }
 }
